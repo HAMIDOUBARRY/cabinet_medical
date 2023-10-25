27dc80a7e587f43eca87c8127ccd6baa0cc5961f
BARRY <Barry@gmail.com>
2023-10-25 17:38:36 +0000

    til

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>title</title>
 </head>
 <body>
     <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eum, nam ullam, eveniet reiciendis voluptate harum assumenda eaque inventore tempora sed fugiat deserunt velit pariatur facere voluptatum alias mollitia neque.</h1>
